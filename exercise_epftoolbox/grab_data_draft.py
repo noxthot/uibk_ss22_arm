83eff4406b850443e12cf00fc48232199b010c28
Gregor Ehrensperger <gregor.ehrensperger@uibk.ac.at>
2022-06-09 10:36:52 +0200

    chore: use 04.01.2016 - 31.12.2017 as test set

--- exercise_epftoolbox/grab_data_draft.py
+++ exercise_epftoolbox/grab_data_draft.py
@@ -1,5 +1,5 @@
 from epftoolbox.data import read_data
 
-df_train, df_test = read_data(path=".", dataset='DE', begin_test_date='01-01-2017', end_test_date='31-12-2017')
+df_train, df_test = read_data(path=".", dataset='DE', begin_test_date='04-01-2016', end_test_date='31-12-2017')
 df_train.to_csv('DE_train.csv')
 df_test.to_csv('DE_test.csv')
