b0ecc8efcee543320f8ca77e4f09cac851fd590f
Gregor Ehrensperger <gregor.ehrensperger@uibk.ac.at>
2022-06-17 12:39:45 +0200

    feat(exercise): also download forecasts

--- exercise_epftoolbox/grab_data_draft.py
+++ exercise_epftoolbox/grab_data_draft.py
@@ -1,5 +1,16 @@
+import os
+
+import pandas as pd
+
 from epftoolbox.data import read_data
 
+prj_dir = "exercise_epftoolbox"
+
 df_train, df_test = read_data(path=".", dataset='DE', begin_test_date='04-01-2016', end_test_date='31-12-2017')
-df_train.to_csv('DE_train.csv')
-df_test.to_csv('DE_test.csv')
+df_train.to_csv(os.path.join(prj_dir, 'DE_train.csv'))
+df_test.to_csv(os.path.join(prj_dir, 'DE_test.csv'))
+
+forecast_url = "https://raw.githubusercontent.com/jeslago/epftoolbox/master/forecasts/Forecasts_DE_DNN_LEAR_ensembles.csv"
+
+forecast = pd.read_csv(forecast_url, index_col=0)
+forecast.to_csv(os.path.join(prj_dir, "DE_test_forecasts.csv"))
