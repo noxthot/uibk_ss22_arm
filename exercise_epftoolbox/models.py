ec829334ea27e35f507d49c201d6d36ea53f846a
Gregor Ehrensperger <gregor.ehrensperger@uibk.ac.at>
2022-06-29 10:39:09 +0200

    feat: add possibility to remove outliers

--- exercise_epftoolbox/models.py
+++ exercise_epftoolbox/models.py
@@ -1,6 +1,7 @@
 #%%
 scalemethod = "meanstd"       # possibilites: minmax, meanstd
-modelchoice = "lasso"      # possibilities: adaboost, elastic, knn, lasso, lightgbm, nn, svr
+modelchoice = "gradientboost"      # possibilities: adaboost, elastic, knn, lasso, lightgbm, nn, svr
+remove_outliers = True
 
 #%%
 import h5py
@@ -39,6 +40,25 @@ with h5py.File(filepath, "r") as f:
 
 X_train = data["xtrain"]
 y_train = data["ytrain"]
+
+if remove_outliers:
+    for col in y_train.columns:
+        q10, q90 = np.percentile(y_train[col], [10, 90])
+        intr_qr = q90 - q10
+    
+        max = q90 + (1.5 * intr_qr)
+        min = q10 - (1.5 * intr_qr)
+    
+        y_train[col][y_train[col] < min] = np.nan
+        y_train[col][y_train[col] > max] = np.nan
+
+    rowswithna = y_train.isna().any(axis=1)
+
+    print(f"Drop {sum(rowswithna)} rows")
+    X_train = X_train[~rowswithna]
+    y_train = y_train[~rowswithna]
+
+
 X_test = data["xtest"]
 y_test = data["ytest"]
 df_forecasts = data["forecasts"]
