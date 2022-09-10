dc0bd5a40a7e9311497cb897fa6ddb874df203c5
Gregor Ehrensperger <gregor.uibk@ehrensperger.dev>
2022-09-11 00:49:25 +0200

    fix(etl): filter for correct data (as described in paper)

--- exercise_epftoolbox/grab_data.py
+++ exercise_epftoolbox/grab_data.py
@@ -7,7 +7,7 @@ from epftoolbox.data import read_data
 prj_dir = "exercise_epftoolbox"
 
 df_train, df_test = read_data(path=".", dataset='DE', begin_test_date='04-01-2016', end_test_date='31-12-2017')
-df_train.to_csv(os.path.join(prj_dir, 'DE_train.csv'))
+df_train.query("index >= '2013-01-10'").to_csv(os.path.join(prj_dir, 'DE_train.csv'))
 df_test.to_csv(os.path.join(prj_dir, 'DE_test.csv'))
 
 forecast_url = "https://raw.githubusercontent.com/jeslago/epftoolbox/master/forecasts/Forecasts_DE_DNN_LEAR_ensembles.csv"
